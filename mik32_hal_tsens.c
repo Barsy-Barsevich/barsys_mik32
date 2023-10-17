cf15827367410bf1450620a5f99dac4a473ad6b3
Barsy_Barsevich <116821312+Barsy-Barsevich@users.noreply.github.com>
2023-10-17 19:28:17 +0500

    v1.2

--- mik32_hal_tsens.c
+++ mik32_hal_tsens.c
@@ -1,5 +1,10 @@
 #include "mik32_hal_tsens.h"
 
+/*!
+@brief  Функция начальной инициализации термосенсора
+@param  нет
+@return нет
+*/
 void HAL_TSENS_Init()
 {
     /* Тактирование аналогового блока */
@@ -16,15 +21,59 @@ void HAL_TSENS_Init()
     HAL_TSENS_Clock(50000ul);             //< Предустановка частоты tsens
 }
 
+/*!
+@brief  Функция установки источника тактирования термосенсора
+@param  TSENS_SYS_CLK         - системная тактовая частота
+        TSENS_HCLK            - частота шины АНВ
+        TSENS_EXTERNAL_32MHZ  - внешний генератор 32МГц
+        TSENS_HSI32M          - внутренний генератор 32МГц
+        TSENS_EXTERNAL_32KHZ  - внешний генератор 32кГц
+        TSENS_LSI32K          - внутренний генератор 32кГц
+@return 
+    true, если источник тактирования установлен успешно
+    false, если входной параметр некорректен или если выбираемая частота меньше минимальной
+*/
 bool HAL_TSENS_ClockSource(uint8_t data)
 {
-    if (data > 0x05) return false;
+    uint32_t f_real;
+    switch (data)
+    {
+        case 0: //TSENS_SYS_CLK
+            f_real = F_CPU / (PM->DIV_AHB + 1) / (PM->DIV_APB_P + 1);
+            break;
+        case 1: //TSENS_HCLK
+            f_real = F_CPU / (PM->DIV_APB_P + 1);
+            break;
+        case 2: //TSENS_EXTERNAL_32MHZ
+            f_real = 32000000UL;
+            break;
+        case 3: //TSENS_HSI32M
+            f_real = 32000000UL;
+            break;
+        case 4: //TSENS_EXTERNAL_32KHZ
+            f_real = 32000UL;
+            break;
+        case 5: //TSENS_LSI32K
+            f_real = 32000UL;
+            break;
+        default:
+            return false;
+    }
+    if (f_real < 32000UL) return false;
     _hal_tsens_clkmux_ = data;
     ANALOG_REG->TSENS_CFG &= ~TSENS_CFG_CLK_MUX_M;
     ANALOG_REG->TSENS_CFG |= (_hal_tsens_clkmux_ << TSENS_CFG_CLK_MUX_S);
-    return HAL_TSENS_Clock(50000ul);  //< Предустановка частоты tsens
+    HAL_TSENS_Clock(50000ul);  //< Предустановка частоты tsens
+    return true;
 }
 
+/*!
+@brief  Функция установки делителя частоты для термосенсора
+@param  делитель частоты (uint16_t)
+@return 
+    true, если делитель частоты установлен успешно
+    false, если делитель частоты слишком мал или слишком велик
+*/
 bool HAL_TSENS_ClockDivider(uint16_t data)
 {
     data &= 0x3FF;
@@ -66,6 +115,13 @@ bool HAL_TSENS_ClockDivider(uint16_t data)
     return true;
 }
 
+/*!
+@brief  Функция установки тактовой частоты для термосенсора
+@param  тактовая частота термосенсора (uint32_t)
+@return 
+    true, если частота установлена успешно
+    false, если введенная частота не попадает в область допустимых значений частот для сенсора
+*/
 bool HAL_TSENS_Clock(uint32_t f_enter)
 {
     if ((f_enter > 100000UL)||(f_enter < 32000UL)) return false;
@@ -114,6 +170,12 @@ bool HAL_TSENS_Clock(uint32_t f_enter)
     return true;
 }
 
+/*!
+@brief  Функция установки калибровочного значения для термосенсора
+@param  калибровочное значение (uint8_t)
+@return нет
+!!! Функция работает только с MIK32V0 !!!
+*/
 #ifdef MIK32V0
 void HAL_TSENS_SetTrim(uint8_t data)
 {
@@ -122,17 +184,31 @@ void HAL_TSENS_SetTrim(uint8_t data)
 }
 #endif
 
+/*!
+@brief  Функция включения режима непрерывных измерений
+@param  нет
+@return нет
+*/
 void HAL_TSENS_ContiniousOn()
 {
     ANALOG_REG->TSENS_CONTINUOUS |= 1;
 }
 
+/*!
+@brief  Функция отключения режима непрерывных измерений
+@param  нет
+@return нет
+*/
 void HAL_TSENS_ContiniousOff()
 {
     ANALOG_REG->TSENS_CONTINUOUS &= ~1;
 }
 
-// Setting Threshold values
+/*!
+@brief  Функция установки значения LOW_THRESHOLD
+@param  значение LOW_THRESHOLD (uint16_t)
+@return нет
+*/
 void HAL_TSENS_SetLowThreshold(uint16_t data)
 {
     data &= 0x3FF;
@@ -141,6 +217,11 @@ void HAL_TSENS_SetLowThreshold(uint16_t data)
     ANALOG_REG->TSENS_THRESHOLD |= ((uint32_t)data << TSENS_TRESHOLD_LOW_S);
 }
 
+/*!
+@brief  Функция установки значения HI_THRESHOLD
+@param  значение HI_THRESHOLD (uint16_t)
+@return нет
+*/
 void HAL_TSENS_SetHiThreshold(uint16_t data)
 {
     data &= 0x3FF;
@@ -149,77 +230,149 @@ void HAL_TSENS_SetHiThreshold(uint16_t data)
     ANALOG_REG->TSENS_THRESHOLD |= ((uint32_t)data << TSENS_TRESHOLD_HI_S);
 }
 
-// Low threshold event
+/*!
+@brief  Функция разрешения прерываний по вектору LOW_THRESHOLD_IRQ
+@param  нет
+@return нет
+*/
 void HAL_TSENS_LowIrq_Enable()
 {
     ANALOG_REG->TSENS_IRQ |= TSENS_IRQ_LOW_MASK_M;
 }
 
+/*!
+@brief  Функция запрета прерываний по вектору LOW_THRESHOLD_IRQ
+@param  нет
+@return нет
+*/
 void HAL_TSENS_LowIrq_Disable()
 {
     ANALOG_REG->TSENS_IRQ &= ~TSENS_IRQ_LOW_MASK_M;
 }
 
+/*!
+@brief  Функция очистки флага прерывания LOW_THRESHOLD_IRQ
+@param  нет
+@return нет
+*/
 void HAL_TSENS_LowIrq_Clear()
 {
     ANALOG_REG->TSENS_CLEAR_IRQ = TSENS_CLEAR_IRQ_LOW_CLEAR_M;
 }
 
+/*!
+@brief  Функция проверки флага прерывания LOW_THRESHOLD_IRQ
+@param  нет
+@return true  - флаг установлен
+        false - флаг очищен
+*/
 bool HAL_TSENS_LowIrq_Event()
 {
     return ANALOG_REG->TSENS_IRQ & TSENS_IRQ_LOW_IRQ_M;
 }
 
-// High threshold event
+/*!
+@brief  Функция разрешения прерываний по вектору HI_THRESHOLD_IRQ
+@param  нет
+@return нет
+*/
 void HAL_TSENS_HiIrq_Enable()
 {
     ANALOG_REG->TSENS_IRQ |= TSENS_IRQ_HI_MASK_M;
 }
+
+/*!
+@brief  Функция запрета прерываний по вектору HI_THRESHOLD_IRQ
+@param  нет
+@return нет
+*/
 void HAL_TSENS_HiIrq_Disable()
 {
     ANALOG_REG->TSENS_IRQ &= ~TSENS_IRQ_HI_MASK_M;
 }
+
+/*!
+@brief  Функция очистки флага прерывания HI_THRESHOLD_IRQ
+@param  нет
+@return нет
+*/
 void HAL_TSENS_HiIrq_Clear()
 {
     ANALOG_REG->TSENS_CLEAR_IRQ = TSENS_CLEAR_IRQ_HI_CLEAR_M;
 }
 
+/*!
+@brief  Функция проверки флага прерывания HI_THRESHOLD_IRQ
+@param  нет
+@return true  - флаг установлен
+        false - флаг очищен
+*/
 bool HAL_TSENS_HiIrq_Event()
 {
     return ANALOG_REG->TSENS_IRQ & TSENS_IRQ_HI_IRQ_M;
 }
 
-// EOS event
-void HAL_TSENS_EOSIrq_Enable()
+/*!
+@brief  Функция разрешения прерываний по вектору EOC_IRQ
+@param  нет
+@return нет
+*/
+void HAL_TSENS_EOCIrq_Enable()
 {
     ANALOG_REG->TSENS_IRQ |= TSENS_IRQ_EOC_MASK_M;
 }
 
-void HAL_TSENS_EOSIrq_Disable()
+/*!
+@brief  Функция запрета прерываний по вектору EOC_IRQ
+@param  нет
+@return нет
+*/
+void HAL_TSENS_EOCIrq_Disable()
 {
     ANALOG_REG->TSENS_IRQ &= ~TSENS_IRQ_EOC_MASK_M;
 }
 
-void HAL_TSENS_EOSIrq_Clear()
+/*!
+@brief  Функция очистки флага прерывания EOC_IRQ
+@param  нет
+@return нет
+*/
+void HAL_TSENS_EOCIrq_Clear()
 {
     ANALOG_REG->TSENS_CLEAR_IRQ = TSENS_CLEAR_IRQ_EOC_CLEAR_M;
 }
 
-bool HAL_TSENS_EOSIrq_Event()
+/*!
+@brief  Функция проверки флага прерывания EOC_IRQ
+@param  нет
+@return true  - флаг установлен
+        false - флаг очищен
+*/
+bool HAL_TSENS_EOCIrq_Event()
 {
     return ANALOG_REG->TSENS_IRQ & TSENS_IRQ_EOC_IRQ_M;
 }
 
 // Reading data
+/*!
+@brief  Функция чтения данных термосенсора в режиме Continious-mode
+@param  нет
+@return температура в градусах Цельсия
+*/
 float HAL_TSENS_ReadMeasurement()
 {
     uint32_t value = ANALOG_REG->TSENS_VALUE & 0x3FF;
     return (float)TSENS_VALUE_TO_CELSIUS(value) / 100.0;
 }
 
+/*!
+@brief  Функция чтения данных термосенсора в режиме Single-mode
+@param  нет
+@return температура в градусах Цельсия
+*/
 float HAL_TSENS_SingleMeasurement()
 {
     ANALOG_REG->TSENS_SINGLE = 1;
-    while (!HAL_TSENS_EOSIrq_Event());
+    while (!HAL_TSENS_EOCIrq_Event());
     return HAL_TSENS_ReadMeasurement();
 }
