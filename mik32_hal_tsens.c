1141143f65b11dee4b1c13a49759ef52650005d5
Barsy_Barsevich <116821312+Barsy-Barsevich@users.noreply.github.com>
2023-10-17 18:32:37 +0500

    Update mik32_hal_tsens.c
    v2 test

--- mik32_hal_tsens.c
+++ mik32_hal_tsens.c
@@ -5,22 +5,24 @@ void HAL_TSENS_Init()
     /* Тактирование аналогового блока */
 	PM->CLK_APB_P_SET = PM_CLOCK_APB_P_ANALOG_REGS_M;
     /* Включение, тактирование температурного сенсора, отключение режима сброса */
-    ANALOG_REG->TSENS_CFG = 	(1<<TSENS_CFG_NRST_S)|
-								(1<<TSENS_CFG_NPD_CLK_S)|
-								(1<<TSENS_CFG_NPD_S);
+    ANALOG_REG->TSENS_CFG = (1<<TSENS_CFG_NRST_S)|
+							(1<<TSENS_CFG_NPD_CLK_S)|
+							(1<<TSENS_CFG_NPD_S);
     /* Предустановка значений default */
+#ifdef MIK32V0
+    HAL_TSENS_SetTrim(128);               //< Настройка Trim только для MIK32V0
+#endif
     HAL_TSENS_ClockSource(TSENS_SYS_CLK); //< Источник такта - sys_clk
-    HAL_TSENS_ClockDivider(499); //< Делитель 500
-    HAL_TSENS_SetTrim(128); //< Настройка Trim = 128
+    HAL_TSENS_Clock(50000ul);             //< Предустановка частоты tsens
 }
 
 bool HAL_TSENS_ClockSource(uint8_t data)
 {
     if (data > 0x05) return false;
     _hal_tsens_clkmux_ = data;
-    ANALOG_REG->TSENS_CFG &= 0b11111111111111111100011111111111;
-    ANALOG_REG->TSENS_CFG |= (_hal_tsens_clkmux_ << 11);
-    return true;
+    ANALOG_REG->TSENS_CFG &= ~TSENS_CFG_CLK_MUX_M;
+    ANALOG_REG->TSENS_CFG |= (_hal_tsens_clkmux_ << TSENS_CFG_CLK_MUX_S);
+    return HAL_TSENS_Clock(50000ul);  //< Предустановка частоты tsens
 }
 
 bool HAL_TSENS_ClockDivider(uint16_t data)
@@ -45,17 +47,21 @@ bool HAL_TSENS_ClockDivider(uint16_t data)
             if ((data < 320)||(data > 1000)) return false;
             break;
         case 4: //TSENS_EXTERNAL_32KHZ
-            if (data != 0) return false;
             data = 0;
             break;
         case 5: //TSENS_LSI32K
-            if (data != 0) return false;
             data = 0;
             break;
         default:
             return false;
     }
+/*#ifdef MIK32V0
     ANALOG_REG->TSENS_CFG &= 0b11111111000000000011111111111111;
+#endif
+#ifdef MIK32V2
+    ANALOG_REG->TSENS_CFG &= 0b11111111111111110000000000111111;
+#endif*/
+    ANALOG_REG->TSENS_CFG &= ~TSENS_CFG_DIV_M;
     ANALOG_REG->TSENS_CFG |= (data<<TSENS_CFG_DIV_S);
     return true;
 }
@@ -88,8 +94,8 @@ bool HAL_TSENS_Clock(uint32_t f_enter)
         default:
             return false;
     }
-    float up_border = f_real / 100000UL;
-    float down_border = f_real / 32000UL;
+    float up_border = (float)f_real / 100000.0;
+    float down_border = (float)f_real / 32000.0;
     // If f_real < 32 kHz, return false
     if (down_border < 1) return false;
     uint32_t del = f_real / f_enter;
@@ -97,16 +103,24 @@ bool HAL_TSENS_Clock(uint32_t f_enter)
     if ((float)del > down_border) del -= 1;
     del -= 1;
     // на вывод
+/*#ifdef MIK32V0
     ANALOG_REG->TSENS_CFG &= 0b11111111000000000011111111111111;
+#endif
+#ifdef MIK32V2
+    ANALOG_REG->TSENS_CFG &= 0b11111111111111110000000000111111;
+#endif*/
+    ANALOG_REG->TSENS_CFG &= (~TSENS_CFG_DIV_M);
     ANALOG_REG->TSENS_CFG |= (del<<TSENS_CFG_DIV_S);
     return true;
 }
 
+#ifdef MIK32V0
 void HAL_TSENS_SetTrim(uint8_t data)
 {
     ANALOG_REG->TSENS_CFG &= 0b11111111111111111111100000000111;
-    ANALOG_REG->TSENS_CFG |= (data << TSENS_CFG_TRIM_S);
+    ANALOG_REG->TSENS_CFG |= (data << 3/*TSENS_CFG_TRIM_S*/);
 }
+#endif
 
 void HAL_TSENS_ContiniousOn()
 {
@@ -122,15 +136,17 @@ void HAL_TSENS_ContiniousOff()
 void HAL_TSENS_SetLowThreshold(uint16_t data)
 {
     data &= 0x3FF;
-    ANALOG_REG->TSENS_THRESHOLD &= 0b11111111111100000000001111111111;
-    ANALOG_REG->TSENS_THRESHOLD |= ((uint32_t)data << 10);
+    //ANALOG_REG->TSENS_THRESHOLD &= 0b11111111111100000000001111111111;
+    ANALOG_REG->TSENS_THRESHOLD &= ~TSENS_TRESHOLD_LOW_M;
+    ANALOG_REG->TSENS_THRESHOLD |= ((uint32_t)data << TSENS_TRESHOLD_LOW_S);
 }
 
 void HAL_TSENS_SetHiThreshold(uint16_t data)
 {
     data &= 0x3FF;
-    ANALOG_REG->TSENS_THRESHOLD &= 0b11111111111111111111110000000000;
-    ANALOG_REG->TSENS_THRESHOLD |= (uint32_t)data;
+    //ANALOG_REG->TSENS_THRESHOLD &= 0b11111111111111111111110000000000;
+    ANALOG_REG->TSENS_THRESHOLD &= ~TSENS_TRESHOLD_HI_M;
+    ANALOG_REG->TSENS_THRESHOLD |= ((uint32_t)data << TSENS_TRESHOLD_HI_S);
 }
 
 // Low threshold event
@@ -197,14 +213,13 @@ bool HAL_TSENS_EOSIrq_Event()
 // Reading data
 float HAL_TSENS_ReadMeasurement()
 {
-    uint32_t value = ANALOG_REG->TSENS_VALUE & 0x3FF; //Vydeliajem 10 bit
-    return (((float)value * 619.2F) / 1024.0F) - 273.15F;
+    uint32_t value = ANALOG_REG->TSENS_VALUE & 0x3FF;
+    return (float)TSENS_VALUE_TO_CELSIUS(value) / 100.0;
 }
 
 float HAL_TSENS_SingleMeasurement()
 {
     ANALOG_REG->TSENS_SINGLE = 1;
-    //while (!(ANALOG_REG->TSENS_IRQ & (1<<TSENS_IRQ_EOS_S)));
     while (!HAL_TSENS_EOSIrq_Event());
     return HAL_TSENS_ReadMeasurement();
 }
