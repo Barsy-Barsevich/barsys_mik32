86fed2a0ce1e9eaf6bcd299bee2ce4cde99eab0e
Barsy_Barsevich <116821312+Barsy-Barsevich@users.noreply.github.com>
2024-03-07 16:37:43 +0300

    Update SPI_Driver.c

--- mpu/SPI_Driver.c
+++ mpu/SPI_Driver.c
@@ -1,14 +1,11 @@
-#ifndef MPU9250_SPI_Driver
-#define MPU9250_SPI_Driver
-
+#include "MPU9250.h"
 
 void SPI_init(MPU_HandleTypeDef* local, SPI_HandleTypeDef* _spi, uint8_t _cs);
-inline void SPI_down(MPU_HandleTypeDef* local);
-inline void SPI_up(MPU_HandleTypeDef* local);
-inline void SPI_writeReg(MPU_HandleTypeDef* local, uint8_t addr, uint8_t data);
-inline void SPI_readReg(MPU_HandleTypeDef* local, uint8_t addr, uint8_t* buf);
-inline void SPI_read14Bytes(MPU_HandleTypeDef* local, uint8_t addr, uint8_t[] buf);
-
+void SPI_down(MPU_HandleTypeDef* local);
+void SPI_up(MPU_HandleTypeDef* local);
+void SPI_writeReg(MPU_HandleTypeDef* local, uint8_t addr, uint8_t data);
+void SPI_readReg(MPU_HandleTypeDef* local, uint8_t addr, uint8_t* buf);
+void SPI_read14Bytes(MPU_HandleTypeDef* local, uint8_t addr, uint8_t* buf);
 
 void SPI_init(MPU_HandleTypeDef* local, SPI_HandleTypeDef* _spi, uint8_t _cs)
 {
@@ -16,24 +13,24 @@ void SPI_init(MPU_HandleTypeDef* local, SPI_HandleTypeDef* _spi, uint8_t _cs)
     local->cs = _cs;
 }
 
-inline void SPI_down(MPU_HandleTypeDef* local)
+void SPI_down(MPU_HandleTypeDef* local)
 {
     HAL_SPI_CS_Enable(local->spi, local->cs);
 }
-inline void SPI_up(MPU_HandleTypeDef* local)
+void SPI_up(MPU_HandleTypeDef* local)
 {
-    HAL_SPI_CS_Disable(local->spi, local->cs);
+    HAL_SPI_CS_Disable(local->spi);
 }
-inline void SPI_writeReg(MPU_HandleTypeDef* local, uint8_t addr, uint8_t data)
+void SPI_writeReg(MPU_HandleTypeDef* local, uint8_t addr, uint8_t data)
 {
     addr &= 0b01111111;
     uint8_t dummy;
     HAL_SPI_CS_Enable(local->spi, local->cs);
     HAL_SPI_Exchange(local->spi, &addr, &dummy, 1, SPI_TIMEOUT_DEFAULT);
     HAL_SPI_Exchange(local->spi, &data, &dummy, 1, SPI_TIMEOUT_DEFAULT);
-    HAL_SPI_CS_Disable(local->spi, local->cs);
+    HAL_SPI_CS_Disable(local->spi);
 }
-inline void SPI_readReg(MPU_HandleTypeDef* local, uint8_t addr, uint8_t* buf)
+void SPI_readReg(MPU_HandleTypeDef* local, uint8_t addr, uint8_t* buf)
 {
     addr &= 0b01111111;
     addr |= 0b10000000;
@@ -41,9 +38,9 @@ inline void SPI_readReg(MPU_HandleTypeDef* local, uint8_t addr, uint8_t* buf)
     HAL_SPI_CS_Enable(local->spi, local->cs);
     HAL_SPI_Exchange(local->spi, &addr, &dummy, 1, SPI_TIMEOUT_DEFAULT);
     HAL_SPI_Exchange(local->spi, &dummy, buf, 1, SPI_TIMEOUT_DEFAULT);
-    HAL_SPI_CS_Disable(local->spi, local->cs);
+    HAL_SPI_CS_Disable(local->spi);
 }
-inline void SPI_read14Bytes(MPU_HandleTypeDef* local, uint8_t addr, uint8_t[] buf)
+void SPI_read14Bytes(MPU_HandleTypeDef* local, uint8_t addr, uint8_t* buf)
 {
     addr &= 0b01111111;
     addr |= 0b10000000;
@@ -51,7 +48,5 @@ inline void SPI_read14Bytes(MPU_HandleTypeDef* local, uint8_t addr, uint8_t[] bu
     HAL_SPI_CS_Enable(local->spi, local->cs);
     HAL_SPI_Exchange(local->spi, &addr, dummy, 1, SPI_TIMEOUT_DEFAULT);
     HAL_SPI_Exchange(local->spi, buf, dummy, 14, SPI_TIMEOUT_DEFAULT);
-    HAL_SPI_CS_Disable(local->spi, local->cs);
+    HAL_SPI_CS_Disable(local->spi);
 }
-
-#endif
