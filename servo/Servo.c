b4994dcd97e8a395f8dd397b486cf0e50af3ae9b
Barsy_Barsevich <116821312+Barsy-Barsevich@users.noreply.github.com>
2024-04-04 15:10:20 +0300

    Update Servo.c

--- servo/Servo.c
+++ servo/Servo.c
@@ -1,20 +1,20 @@
-#include "Servo.h"
+#include "Servo32.h"
 
 void Servo_ini(HAL_ServoPinTypeDef pin)
 {
     switch (pin)
     {
-        case ServoGPIO0_7:  timer16_ini(TIMER16_0); break;
-        case ServoGPIO0_10: timer16_ini(TIMER16_1); break;
-        case ServoGPIO0_13: timer16_ini(TIMER16_2); break;
-        case ServoGPIO0_0:  timer32_ini(TIMER32_1,0); break;
-        case ServoGPIO0_1:  timer32_ini(TIMER32_1,1); break;
-        case ServoGPIO0_2:  timer32_ini(TIMER32_1,2); break;
-        case ServoGPIO0_3:  timer32_ini(TIMER32_1,3); break;
-        case ServoGPIO1_0:  timer32_ini(TIMER32_2,0); break;
-        case ServoGPIO1_1:  timer32_ini(TIMER32_2,1); break;
-        case ServoGPIO1_2:  timer32_ini(TIMER32_2,2); break;
-        case ServoGPIO1_3:  timer32_ini(TIMER32_2,3); break;
+        case ServoPin_0_7:  timer16_ini(TIMER16_0); break;
+        case ServoPin_0_10: timer16_ini(TIMER16_1); break;
+        case ServoPin_0_13: timer16_ini(TIMER16_2); break;
+        case ServoPin_0_0:  timer32_ini(TIMER32_1,0); break;
+        case ServoPin_0_1:  timer32_ini(TIMER32_1,1); break;
+        case ServoPin_0_2:  timer32_ini(TIMER32_1,2); break;
+        case ServoPin_0_3:  timer32_ini(TIMER32_1,3); break;
+        case ServoPin_1_0:  timer32_ini(TIMER32_2,0); break;
+        case ServoPin_1_1:  timer32_ini(TIMER32_2,1); break;
+        case ServoPin_1_2:  timer32_ini(TIMER32_2,2); break;
+        case ServoPin_1_3:  timer32_ini(TIMER32_2,3); break;
     }
 }
 
@@ -93,7 +93,7 @@ static HAL_StatusTypeDef timer16_ini(TIMER16_TypeDef* timer)
     local_timer16->Instance = timer;
     local_timer16->Clock.Source = TIMER16_SOURCE_INTERNAL_SYSTEM;
     local_timer16->CountMode = TIMER16_COUNTMODE_INTERNAL;  /* При тактировании от Input1 не имеет значения */
-    local_timer16->Clock.Prescaler = TIMER16_PRESCALER_16;
+    local_timer16->Clock.Prescaler = SERVO_TIMER16_PRESCALER;
     local_timer16->ActiveEdge = TIMER16_ACTIVEEDGE_RISING;  /* Выбирается при тактировании от Input1 */
     local_timer16->Preload = TIMER16_PRELOAD_AFTERWRITE;
     local_timer16->Trigger.Source = TIMER16_TRIGGER_TIM1_GPIO1_9; 
@@ -116,16 +116,77 @@ void Servo32_PrintAngle(HAL_ServoPinTypeDef pin, uint8_t angle)
     uint16_t ocr = SERVO_0DEG_VALUE+(SERVO_ANGLE_TO_VALUE_COEF*angle);
     switch (pin)
     {
-        case ServoGPIO0_7:  HAL_Timer16_SetCMP(&servo_timer16_0, ocr); break;
-        case ServoGPIO0_10: HAL_Timer16_SetCMP(&servo_timer16_1, ocr); break;
-        case ServoGPIO0_13: HAL_Timer16_SetCMP(&servo_timer16_2, ocr); break;
-        case ServoGPIO0_0:  HAL_Timer32_Channel_OCR_Set(&servo_tim32_1_ch0, ocr); break;
-        case ServoGPIO0_1:  HAL_Timer32_Channel_OCR_Set(&servo_tim32_1_ch1, ocr); break;
-        case ServoGPIO0_2:  HAL_Timer32_Channel_OCR_Set(&servo_tim32_1_ch2, ocr); break;
-        case ServoGPIO0_3:  HAL_Timer32_Channel_OCR_Set(&servo_tim32_1_ch3, ocr); break;
-        case ServoGPIO1_0:  HAL_Timer32_Channel_OCR_Set(&servo_tim32_2_ch0, ocr); break;
-        case ServoGPIO1_1:  HAL_Timer32_Channel_OCR_Set(&servo_tim32_2_ch1, ocr); break;
-        case ServoGPIO1_2:  HAL_Timer32_Channel_OCR_Set(&servo_tim32_2_ch2, ocr); break;
-        case ServoGPIO1_3:  HAL_Timer32_Channel_OCR_Set(&servo_tim32_2_ch3, ocr); break;
+        case ServoPin_0_7:  HAL_Timer16_SetCMP(&servo_timer16_0, ocr); break;
+        case ServoPin_0_10: HAL_Timer16_SetCMP(&servo_timer16_1, ocr); break;
+        case ServoPin_0_13: HAL_Timer16_SetCMP(&servo_timer16_2, ocr); break;
+        case ServoPin_0_0:  HAL_Timer32_Channel_OCR_Set(&servo_tim32_1_ch0, ocr); break;
+        case ServoPin_0_1:  HAL_Timer32_Channel_OCR_Set(&servo_tim32_1_ch1, ocr); break;
+        case ServoPin_0_2:  HAL_Timer32_Channel_OCR_Set(&servo_tim32_1_ch2, ocr); break;
+        case ServoPin_0_3:  HAL_Timer32_Channel_OCR_Set(&servo_tim32_1_ch3, ocr); break;
+        case ServoPin_1_0:  HAL_Timer32_Channel_OCR_Set(&servo_tim32_2_ch0, ocr); break;
+        case ServoPin_1_1:  HAL_Timer32_Channel_OCR_Set(&servo_tim32_2_ch1, ocr); break;
+        case ServoPin_1_2:  HAL_Timer32_Channel_OCR_Set(&servo_tim32_2_ch2, ocr); break;
+        case ServoPin_1_3:  HAL_Timer32_Channel_OCR_Set(&servo_tim32_2_ch3, ocr); break;
     }
+}
+
+HAL_StatusTypeDef Servo_Clock_ini()
+{
+    uint32_t _0deg_value, _90deg_value, _180deg_value;
+    uint32_t timer_prescaler, timer_freq;
+    HAL_Timer16_PrescalerTypeDef timer16_prescaler;
+    uint32_t apb_p_freq = HAL_PCC_GetSysClockFreq() / (PM->DIV_AHB * PM->DIV_APB_P);
+#ifdef SERVO_50HZ
+    if (apb_p_freq <= 2000000UL)
+    {
+        timer_prescaler = 1;
+        timer16_prescaler = TIMER16_PRESCALER_1;
+    }
+    else if (apb_p_freq <= 4000000UL)
+    {
+        timer_prescaler = 2;
+        timer16_prescaler = TIMER16_PRESCALER_2;
+    }
+    else if (apb_p_freq <= 8000000UL)
+    {
+        timer_prescaler = 4;
+        timer16_prescaler = TIMER16_PRESCALER_4;
+    }
+    else if (apb_p_freq <= 16000000UL)
+    {
+        timer_prescaler = 8;
+        timer16_prescaler = TIMER16_PRESCALER_8;
+    }
+    else
+    {
+        timer_prescaler = 16;
+        timer16_prescaler = TIMER16_PRESCALER_16;
+    }
+    timer_freq = apb_p_freq /  timer_prescaler;
+    /* Servo tact = 50Hz, space between 0.5ms & 2.5ms shold have >=256 steps */
+    if (timer_freq < (50*40/4*256)) return HAL_ERROR;
+    _0deg_value = timer_freq/50/40;  //< 0.5ms
+    // _90deg_value = 3 * _0deg_value; //< 1.5ms
+    _180deg_value = 5 * _0deg_value; //< 2.5ms
+#else
+#ifdef SERVO_330HZ
+    if (apb_p_freq <= 16000000UL)
+    {
+        timer_prescaler = 1;
+        timer16_prescaler = TIMER16_PRESCALER_1;
+    }
+    else
+    {
+        timer_prescaler = 2;
+        timer16_prescaler = TIMER16_PRESCALER_2;
+    }
+    timer_freq = apb_p_freq /  timer_prescaler;
+    //Servo tact = 333Hz=(1000/3)Hz, space between 0.5ms & 2.5ms shold have >=256 steps
+    if (timer_freq < (1000*6/3/4*256)) return HAL_ERROR;
+    _0deg_value = timer_freq/(1000*6/3); //0.5ms
+    // _90deg_value = 3 * _0deg_value; //1.5ms
+    _180deg_value = 5 * _0deg_value; //2.5ms
+#endif
+#endif
+    return HAL_OK;
 }
